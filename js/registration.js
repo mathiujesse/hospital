9b6f7c5607e58bd6a19a678706359d3a0e6bc130
Gr3atJes <mathiujes@gmail.com>
2024-04-18 14:00:01 +0300

    may the fourth

--- js/registration.js
+++ js/registration.js
@@ -1,7 +1,11 @@
 function showTooltip() {
             var tooltip = document.getElementById("tooltipText");
-            tooltip.style.visibility = "visible";
+            // tooltip.style.visibility = "visible";
             // Simulate fetching the submission status from a server
             // Replace [status] with the actual submission status retrieved from the server
             tooltip.textContent = "Submission status: Submitted";
+}
+function alert(message) {
+    console.log(message);
+    message.innerHTML = "Error submitting";
 }

05d631fe4ed9c8b08e9ff2dcfeb6ef2acc442855
Gr3atJes <mathiujes@gmail.com>
2024-04-18 03:27:38 +0300

    changed some lines finally

--- js/form.js
+++ js/form.js
@@ -1,7 +1,8 @@
-function showTooltip() {
+ function showTooltip() {
             var tooltip = document.getElementById("tooltipText");
             tooltip.style.visibility = "visible";
             // Simulate fetching the submission status from a server
             // Replace [status] with the actual submission status retrieved from the server
-            tooltip.textContent = "Submission status: Submitted";
-}
+            tooltip.textContent = "Submission status: [status]";
+ }
+ 

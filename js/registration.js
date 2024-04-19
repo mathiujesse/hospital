d5f7324707c012a6b1e8c38c0427547d80661ac5
Gr3atJes <mathiujes@gmail.com>
2024-04-19 11:34:44 +0300

    adding form validation

--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,7 @@
-document.getElementById("go-home-btn").addEventListener("click", function() {
-  window.location.href = "/index.html"; // Redirect to the homepage
-});
+ document.getElementById("myForm").addEventListener("submit", function(event) {
+    var form = event.target;
+    if (!form.checkValidity()) {
+      event.preventDefault(); // Prevent form submission if validation fails
+      alert("Please fill in all required fields.");
+    }
+  });

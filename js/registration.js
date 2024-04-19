52348f9bbd4a66c4bd39337bc09ce8a22cbc38b3
Gr3atJes <mathiujes@gmail.com>
2024-04-19 10:04:40 +0300

    fixing some buttons

--- js/registration.js
+++ js/registration.js
@@ -1,11 +1,3 @@
-function showTooltip() {
-            var tooltip = document.getElementById("tooltipText");
-            // tooltip.style.visibility = "visible";
-            // Simulate fetching the submission status from a server
-            // Replace [status] with the actual submission status retrieved from the server
-            tooltip.textContent = "Submission status: Submitted";
-}
-function alert(message) {
-    console.log(message);
-    message.innerHTML = "Error submitting";
-}
+document.getElementById("go-home-btn").addEventListener("click", function() {
+  window.location.href = "/index.html"; // Redirect to the homepage
+});
